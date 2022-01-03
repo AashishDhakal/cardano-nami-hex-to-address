8d18de939e47dac82878fe7c8788ac0ad91264e9
AashishDhakal <aashish.dhakal@student.ku.edu.np>
2022-01-03 17:32:23 +0545

    updated response

--- routes/decode.js
+++ routes/decode.js
@@ -5,7 +5,9 @@ var cardano = require('@emurgo/cardano-serialization-lib-nodejs');
 /* Decode text. */
 router.get('/:cborHex', function(req, res, next) {
   var cborHex = req.params.cborHex;
-  res.send(cardano.Address.from_bytes(Buffer.from(cborHex, "hex")).to_bech32());
+  res.send({
+    "address": cardano.Address.from_bytes(Buffer.from(cborHex, "hex")).to_bech32()
+  });
 });
 
 module.exports = router;
